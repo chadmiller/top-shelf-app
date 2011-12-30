0d5c1313b6af4381deb43576fe96a1aba00605b6
Chad Miller <chad@>
2011-12-29 21:40:17 -0500

    Enable sharing of shopping list to SMS.

--- src/org/chad/jeejah/library/ShoppingListActivity.java
+++ src/org/chad/jeejah/library/ShoppingListActivity.java
@@ -2,11 +2,15 @@ package org.chad.jeejah.library;
 
 import android.view.View;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.content.DialogInterface;
+import android.content.DialogInterface;
 import android.util.Log;
 import android.text.Html;
 import android.view.Menu;
@@ -18,10 +22,13 @@ import com.markupartist.android.widget.ActionBar;
 import com.markupartist.android.widget.ActionBar.Action;
 import com.markupartist.android.widget.ActionBar.IntentAction;
 
+import java.util.Collection;
+import java.util.Set;
 import java.util.TreeSet;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
 
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 
@@ -34,12 +41,15 @@ public class ShoppingListActivity extends Activity {
 	private static final int MAX_PRODUCTIVE_INGREDIENTS_WANTED = 5;
 
 	private GoogleAnalyticsTracker tracker;
+	private Set<String> candidateSharedIngredients;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.shopping_list);
+
+		candidateSharedIngredients = new TreeSet<String>();
 
 		final ActionBar actionBar = (ActionBar) findViewById(R.id.actionbar);
 		actionBar.setDisplayHomeAsUpEnabled(true);
@@ -83,6 +93,7 @@ public class ShoppingListActivity extends Activity {
 					if (limit-- < 1) break;
 					final int recipeCount = bundle.getStringArray("enabledby "+ingredients[i]).length;
 					final TextView t = new TextView(this);
+					candidateSharedIngredients.add(ingredients[i]);
 					t.setText(Html.fromHtml("\u2022 <b>" + ingredients[i] + "</b> would let you make " + recipeCount + " more recipes."));
 					t.setTextSize(16);
 					t.setPadding(10, 8, 10, 2);
@@ -111,6 +122,7 @@ public class ShoppingListActivity extends Activity {
 
 				for (String ingredient : ingredients) {
 					final TextView t = new TextView(this);
+					//candidateSharedIngredients.add(ingredient);  // TODO  After this list excludes owned, reenable this.
 					t.setText(Html.fromHtml("\u2022 <b>" + ingredient + "</b>"));
 					t.setTextSize(16);
 					t.setPadding(20, 3, 10, 2);
@@ -136,6 +148,7 @@ public class ShoppingListActivity extends Activity {
 				final ArrayList<String> ingredients = bundle.getStringArrayList("ingredients");
 				for (String s : ingredients) {
 					final TextView t = new TextView(this);
+					candidateSharedIngredients.add(s);
 					t.setText(Html.fromHtml("\u2022 <b>" + s + "</b>"));
 					t.setTextSize(16);
 					t.setPadding(20, 3, 10, 2);
@@ -149,6 +162,19 @@ public class ShoppingListActivity extends Activity {
 			}
 
 		}
+
+		class ShareAction implements Action {
+			@Override
+			public int getDrawable() {
+				return android.R.drawable.ic_menu_share;
+			}
+			@Override
+			public void performAction(View view) {
+				showDialog(1);
+			}
+		}
+		actionBar.addAction(new ShareAction());
+
 	}
 
 	@Override
@@ -161,6 +187,7 @@ public class ShoppingListActivity extends Activity {
 		menu.add(Menu.NONE, R.id.instructions, 1, R.string.help).setIcon(android.R.drawable.ic_menu_help);
 		menu.add(Menu.NONE, R.id.feedback, 4, R.string.feedback);
 		menu.add(Menu.NONE, R.id.credits, 5, R.string.credits);
+		menu.add(Menu.NONE, R.id.share, 6, R.string.share);
 		return true;
 	}
 
@@ -182,10 +209,61 @@ public class ShoppingListActivity extends Activity {
 				intent = new Intent(this, Credits.class);
 				startActivity(intent);
 				return true;
+			case R.id.share:
+				showDialog(1);
+				return true;
 			default:
 				return super.onOptionsItemSelected(item);
 		}
 	}
 
+	@Override
+	public Dialog onCreateDialog(int id) {
+		final AlertDialog.Builder adb = new AlertDialog.Builder(this);
+		final String[] items = candidateSharedIngredients.toArray(new String[candidateSharedIngredients.size()]);
+		final Set<String> actualSharingIngredients = new TreeSet<String>(); 
+
+		final DialogInterface.OnMultiChoiceClickListener itemMcl = new DialogInterface.OnMultiChoiceClickListener() {
+			public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+				if (isChecked) {
+					actualSharingIngredients.add(items[which]);
+				} else {
+					actualSharingIngredients.remove(items[which]);
+				}
+			}
+		};
+		adb.setTitle(R.string.share_shopping_title)
+			.setMultiChoiceItems(items, null, itemMcl)
+			.setNegativeButton(R.string.share_cancel, new DialogInterface.OnClickListener() { 
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			}).setPositiveButton(R.string.share_yes, new DialogInterface.OnClickListener() { 
+				public void onClick(DialogInterface dialog, int which) {
+					sendSms(actualSharingIngredients);
+					dialog.dismiss();
+				}
+			});
+		return adb.create();
+	}
+
+	private void sendSms(Set ingredients) {
+		Intent intent = new Intent(Intent.ACTION_VIEW); 
+		intent.putExtra("sms_body", join(ingredients, "; ")); 
+		intent.setType("vnd.android-dir/mms-sms"); 
+		startActivity(intent);
+	}
+
+	public static String join(Collection s, String delimiter) {
+		StringBuffer buffer = new StringBuffer();
+		Iterator iter = s.iterator();
+		while (iter.hasNext()) {
+			buffer.append(iter.next());
+			if (iter.hasNext()) {
+				buffer.append(delimiter);
+			}
+		}
+		return buffer.toString();
+	}
 
 }
